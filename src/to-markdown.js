24d1241c8763ffbe0dc2b99ff33f8df82de20302
Kevin Dew <kevindew@me.com>
2019-03-08 18:29:40 +0000

    Support the abbr element
    As this is not part of commonmark and instead a part of kramdown (as used
    by govspeak) the abbr element needs a specific rule.

--- src/to-markdown.js
+++ src/to-markdown.js
@@ -9,6 +9,29 @@ const service = new TurndownService({
 // stop all escaping
 service.escape = (string) => string
 
+service.addRule('abbr', {
+  filter: (node) => {
+    return node.nodeName.toLowerCase() === 'abbr' && node.getAttribute('title')
+  },
+  replacement: function (content, node) {
+    this.references[content] = node.getAttribute('title')
+    return content
+  },
+  references: {},
+  append: function () {
+    if (Object.keys(this.references).length === 0) {
+      return ''
+    }
+
+    let references = '\n\n'
+    for (const abbr in this.references) {
+      references += `*[${abbr}]: ${this.references[abbr]}\n`
+    }
+    this.references = {} // reset after appending
+    return references
+  }
+})
+
 export default function toMarkdown (html) {
   return service.turndown(html)
 }
--- test/to-markdown.test.js
+++ test/to-markdown.test.js
@@ -20,3 +20,21 @@ it("doesn't escape markdown", () => {
   const html = '<p>[Markdown](link)</p>'
   expect(toMarkdown(html)).toEqual('[Markdown](link)')
 })
+
+it('converts abbr elements to references', () => {
+  const html = `
+    <p>
+      Documents on the web are wrote using
+      <abbr title="Hypertext Markup Language">HTML</abbr> and styled with
+      <abbr title="Cascading Style Sheets">CSS</abbr>. These are both examples
+      of web standards.
+    <p>
+  `
+
+  expect(toMarkdown(html)).toEqual(
+    'Documents on the web are wrote using HTML and styled with CSS. These ' +
+    'are both examples of web standards.\n\n' +
+    '*[HTML]: Hypertext Markup Language\n' +
+    '*[CSS]: Cascading Style Sheets'
+  )
+})
