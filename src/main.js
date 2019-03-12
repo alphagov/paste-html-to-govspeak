f948cde73767e2258a96d79b6a82def03e9177aa
Kevin Dew <kevindew@me.com>
2019-03-12 19:45:35 +0000

    Trigger events during the paste process
    These act as hooks to allow for means to debug this conversion process.
    
    There are 3 events:
    
    htmlinput: provides details of the HTML (if any) that was received,
    always triggered.
    sanitise: provides details of the HMTL once sanitised, only fires if
    HTML is input.
    govspeak: provides details of the converted govspeak, only fires if HTML
    is input.
    
    As IE11 does not support the CustomEvent constructor we use a snippet from the
    polyfill provided on MDN [1]. If the usage of these events is to
    increase in complexity we should use a polyfill from NPM.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill

--- src/main.js
+++ src/main.js
@@ -9,13 +9,34 @@ function htmlFromPasteEvent (event) {
   return event.clipboardData.getData('text/html')
 }
 
+function triggerPasteEvent (element, eventName, detail) {
+  let params = { bubbles: false, cancelable: false, detail: detail || null }
+  let event
+
+  if (typeof window.CustomEvent === 'function') {
+    event = new window.CustomEvent(eventName, params)
+  } else {
+    event = document.createEvent('CustomEvent')
+    event.initCustomEvent(eventName, params.bubbles, params.cancelable, params.detail)
+  }
+
+  element.dispatchEvent(event)
+}
+
 export default function pasteHtmlToGovspeak (event) {
   const element = event.target
 
   const html = htmlFromPasteEvent(event)
+  triggerPasteEvent(element, 'htmlinput', html)
 
   if (html && html.length) {
-    insertTextAtCursor(element, toGovspeak(sanitizeHtml(html)))
+    const sanitised = sanitizeHtml(html)
+    triggerPasteEvent(element, 'sanitise', sanitised)
+
+    const govspeak = toGovspeak(sanitised)
+    triggerPasteEvent(element, 'govspeak', govspeak)
+
+    insertTextAtCursor(element, govspeak)
     event.preventDefault()
   }
 }
--- test/main.test.js
+++ test/main.test.js
@@ -2,10 +2,29 @@
 
 import pasteHtmlToGovspeak from '../src/main'
 
-it("maintains browser default behaviour if HTML isn't pasted", () => {
-  const textarea = document.createElement('textarea')
+let textarea
+
+beforeEach(() => {
+  textarea = document.createElement('textarea')
   textarea.addEventListener('paste', pasteHtmlToGovspeak)
 
+  document.execCommand = jest.fn()
+})
+
+function createHtmlPasteEvent (html) {
+  const event = new window.Event('paste')
+  event.clipboardData = {
+    getData: (type) => {
+      if (type === 'text/html') {
+        return html
+      }
+    }
+  }
+
+  return event
+}
+
+it("maintains browser default behaviour if HTML isn't pasted", () => {
   const event = new window.Event('paste')
   event.preventDefault = jest.fn()
   textarea.dispatchEvent(event)
@@ -14,20 +33,85 @@ it("maintains browser default behaviour if HTML isn't pasted", () => {
 })
 
 it('converts HTML to govspeak if HTML is pasted', () => {
-  const textarea = document.createElement('textarea')
-  textarea.addEventListener('paste', pasteHtmlToGovspeak)
-
-  const event = new window.Event('paste')
-  event.clipboardData = {
-    getData: (type) => {
-      if (type === 'text/html') {
-        return '<h2>Hello</h2>'
-      }
-    }
-  }
-
-  document.execCommand = jest.fn()
-
-  textarea.dispatchEvent(event)
+  textarea.dispatchEvent(createHtmlPasteEvent('<h2>Hello</h2>'))
   expect(textarea.value).toEqual('## Hello')
+})
+
+describe('htmlinput event', () => {
+  it('has raw HTML as the detail if HTML is pasted', () => {
+    const listener = jest.fn()
+    const html = '<script>alert("hi")</script>'
+
+    textarea.addEventListener('htmlinput', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(html))
+
+    expect(listener).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: html
+      })
+    )
+  })
+
+  it('has null as the detail if no HTML is pasted', () => {
+    const listener = jest.fn()
+
+    textarea.addEventListener('htmlinput', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(null))
+
+    expect(listener).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: null
+      })
+    )
+  })
+})
+
+describe('sanitise event', () => {
+  it('has sanitised HTML as the event detail', () => {
+    const listener = jest.fn()
+    const html = '<p onclick="return false">Text</p>'
+
+    textarea.addEventListener('sanitise', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(html))
+
+    expect(listener).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: '<p>Text</p>'
+      })
+    )
+  })
+
+  it("isn't called if no HTML is pasted", () => {
+    const listener = jest.fn()
+
+    textarea.addEventListener('sanitise', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(null))
+
+    expect(listener).not.toHaveBeenCalled()
+  })
+})
+
+describe('govspeak event', () => {
+  it('has govspeak as the event detail', () => {
+    const listener = jest.fn()
+    const html = '<h2>Title</h2>'
+
+    textarea.addEventListener('govspeak', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(html))
+
+    expect(listener).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: '## Title'
+      })
+    )
+  })
+
+  it("isn't called if no HTML is pasted", () => {
+    const listener = jest.fn()
+
+    textarea.addEventListener('govspeak', listener)
+    textarea.dispatchEvent(createHtmlPasteEvent(null))
+
+    expect(listener).not.toHaveBeenCalled()
+  })
 })
