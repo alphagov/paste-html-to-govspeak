bd34a793f00e7c382152c8cd7a947cde1823fa1e
Kevin Dew <kevindew@me.com>
2019-03-15 11:33:01 +0000

    Set up rules that sanitise used to enforce
    This applies the various rules that were previously done through a
    sanitise step.
    
    For links, it removes any empty ones (which is actually new
    functionality) and ignores any title attributes - turndown will use
    these by default which is counter to GOV.UK guidance on markdown links
    
    We only use headings h2 and h3 and turn other ones into paragraphs.
    
    Bold and italic tags are removed - we do support bold on GOV.UK but it's
    usage is discouraged.
    
    Image tags are removed as users should explicitly add an image
    
    and finally empty paragraphs are removed.

--- test/to-govspeak.test.js
+++ test/to-govspeak.test.js
@@ -3,7 +3,21 @@
 import toGovspeak from '../src/to-govspeak'
 
 it('converts HTML to govspeak', () => {
-  expect(toGovspeak('<h2>Hello</h2>')).toEqual('## Hello')
+  expect(toGovspeak('<p>Hello</p>')).toEqual('Hello')
+})
+
+it("doesn't escape markdown", () => {
+  const html = '<p>[Markdown](link)</p>'
+  expect(toGovspeak(html)).toEqual('[Markdown](link)')
+})
+
+it('removes empty links', () => {
+  expect(toGovspeak('<a href="https://www.gov.uk"><a>')).toEqual('')
+})
+
+it('ignores title attributes of links', () => {
+  const html = '<a href="https://www.gov.uk" title="GOV.UK">www.gov.uk<a>'
+  expect(toGovspeak(html)).toEqual('[www.gov.uk](https://www.gov.uk)')
 })
 
 it('converts lists to a dash bullet style', () => {
@@ -16,9 +30,30 @@ it('converts lists to a dash bullet style', () => {
   expect(toGovspeak(html)).toEqual('-   Item 1\n-   Item 2')
 })
 
-it("doesn't escape markdown", () => {
-  const html = '<p>[Markdown](link)</p>'
-  expect(toGovspeak(html)).toEqual('[Markdown](link)')
+it('maintains H2 and H3 headers', () => {
+  expect(toGovspeak('<h2>Hello</h2>')).toEqual('## Hello')
+  expect(toGovspeak('<h3>Hello</h3>')).toEqual('### Hello')
+})
+
+it('strips other headers', () => {
+  expect(toGovspeak('<h1>Hello</h1>')).toEqual('Hello')
+  expect(toGovspeak('<h4>Hello</h4>')).toEqual('Hello')
+  expect(toGovspeak('<h5>Hello</h5>')).toEqual('Hello')
+  expect(toGovspeak('<h6>Hello</h6>')).toEqual('Hello')
+})
+
+it('strips b and strong elements', () => {
+  expect(toGovspeak('<p>With <b>bold</b> text</p>')).toEqual('With bold text')
+  expect(toGovspeak('<p>With <strong>strong</strong> text</p>')).toEqual('With strong text')
+})
+
+it('strips i and em elements', () => {
+  expect(toGovspeak('<p>With <i>italic</i> text</p>')).toEqual('With italic text')
+  expect(toGovspeak('<p>With <em>emphasised</em> text</p>')).toEqual('With emphasised text')
+})
+
+it('removes image elements', () => {
+  expect(toGovspeak('<img src="image.jpg" alt="" />')).toEqual('')
 })
 
 it('converts abbr elements to references', () => {
@@ -36,5 +71,18 @@ it('converts abbr elements to references', () => {
     'are both examples of web standards.\n\n' +
     '*[HTML]: Hypertext Markup Language\n' +
     '*[CSS]: Cascading Style Sheets'
+  )
+})
+
+it('removes empty paragraphs', () => {
+  const html = `
+    <p>Not empty</p>
+    <p><br /></p>
+    <p>  </p>
+    <p>  <br />  </p>
+    <p>Not empty either</p>
+  `
+  expect(toGovspeak(html)).toEqual(
+    'Not empty\n\nNot empty either'
   )
 })
